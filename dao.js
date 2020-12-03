08e6b408028341cf36d4698b09733789fc14bff6
luguangyao <294984687@qq.com>
2020-12-03 20:36:56 +0800

    update

--- dao.js
+++ dao.js
@@ -6,7 +6,7 @@ const pool = mysql.createPool({
     host:'127.0.0.1',
     port:'3306',
     user:'root',
-    password:'12345678',
+    password:'123456',
     database:'scnu',
     connectionLimit:'20'
 
