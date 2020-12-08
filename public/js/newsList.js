27ae972ba6cb9b18117d26b0e8b6e84f5831f89f
luguangyao <294984687@qq.com>
2020-12-08 16:08:20 +0800

    update

--- public/js/newsList.js
+++ public/js/newsList.js
@@ -1,18 +1,19 @@
 newsNum=10;
 newsType=undefined;
 newsData=undefined;
-headNews=[]
+newsHeadId=[];
+headNews=[];
 $(function (){
     $("#navigator").load("http://localhost:3000/template/navigator.html");
     $("#swiper").load("http://localhost:3000/template/swiper.html");
     $("#footer").load("http://localhost:3000/template/footer.html");
 
     var data = window.location.href.split('/').reverse();
-    newsType = data[0];
-    getListData()
+    newsType = data[0].replace("#","");
+    getData()
 })
 
-function getListData(){
+function getData(){
     var xmlhttp;
     if (window.XMLHttpRequest)
     {
@@ -31,9 +32,17 @@ function getListData(){
             res=xmlhttp.responseText;
             newsData=JSON.parse(res);
             setNewsList();
+            newsNum=newsData.length;
+            for(i=newsData.length-1;i>=newsData.length-3;i--){
+                newsHeadId.push(newsData[i]['nid'])
+            }
+            for(nt=0;nt<newsHeadId.length;nt++){
+                getHeadData(nt);
+            }
+            
         }
     }
-    xmlhttp.open("GET",'/'+['data',1,String(newsNum)].join('/'),true);
+    xmlhttp.open("GET",'/'+["data",String(newsType),String(newsNum)].join('/'),true);
     xmlhttp.send();
 }
 
@@ -55,3 +64,50 @@ function setNewsList(){
     
 }
 
+function getHeadData(i){
+    
+        
+        var xmlhttp;
+        if (window.XMLHttpRequest){xmlhttp=new XMLHttpRequest();}
+        else{xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");}
+        xmlhttp.onreadystatechange=function(){  
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            res=xmlhttp.responseText;
+            news=JSON.parse(res)[0];
+            headNews.push(news)
+            setNewsHead(news,i)
+        }
+        }
+        xmlhttp.open("GET",'/'+["data","0",String(newsHeadId[i])].join('/'),true);
+        xmlhttp.send();
+   
+    
+}
+function setNewsHead(news,i){
+    //console.log(news)
+    //console.log(i)
+    $swiperImg=$(".swiperImg")
+    $swiperYear=$(".year")
+    $swiperDayMonth=$(".day_month")
+    $swiperTitle=$(".news-h")
+    $swiperNewsSum=$(".news-sum")
+    $newsHeadA=$(".newsHeadA")
+    
+    title=news["title"]
+    content=news["content"]
+    publishtime=new Date(news["publishtime"])
+    year=publishtime.getFullYear()
+    month=publishtime.getMonth()
+    date=publishtime.getDate()
+    imgName=news["url"]!=undefined?news["url"]:"1606135527117580.png"
+    imgPath="http://localhost:3000/public/image/NEWSimage/"+imgName
+    $($swiperImg[i]).attr("src",imgPath)
+    $($swiperYear[i]).html(String(year))
+    $($swiperDayMonth[i]).html(String(month)+"-"+String(year))
+    $($swiperTitle[i]).html(String(title))
+    $($swiperNewsSum[i]).html(String(content))
+    $($newsHeadA[i]).attr("href")
+    
+
+}
